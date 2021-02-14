026f9a559decf0869d1d1fa940a549fe4bb0f0fb
ASHUTOSH KUMBHAR <ashutoshkumbhar27@gmail.com>
2021-02-14 13:37:34 +0530

    New 2

---
+++ FANG PY/First-Non-Recuring-Charachter.py
@@ -0,0 +1,13 @@
+str = "AABCCDE"
+dic = {}
+for x in str:
+    if x in dic:
+        dic[x] = dic[x] + 1
+    else:
+        dic[x] = 1
+
+print(dic)
+for x in dic:
+    if dic[x]==1:
+        print(x)
+        break
